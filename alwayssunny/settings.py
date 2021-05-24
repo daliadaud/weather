37bed259be6376a0f7a1a66075a99df85db76fb2
dalia daud <daliadaud@gmail.com>
2021-05-24 10:24:42 +0800

    added logging

--- alwayssunny/settings.py
+++ alwayssunny/settings.py
@@ -102,6 +102,31 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# LOGGING
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
+
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
--- weather/utils.py
+++ weather/utils.py
@@ -1,17 +1,30 @@
 import requests
+import logging
+
 from .consts import WEATHER_API_BASE
 
-def get_current_weather(lat, long):
-    weather_url = WEATHER_API_BASE.format(lat=lat, long=long)
-    try:
-        r = requests.get(weather_url)
-        r.raise_for_status()
-        data = r.json()
+
+logger = logging.getLogger(__name__)
+
+
+def get_current_weather(latitute, long):
+    data = request_weather(latitute, long)
+    if data:
         current = data["properties"]["timeseries"][0]
         current_temp = {
             "temperature": current["data"]["instant"]["details"]["air_temperature"]
         }
         return current_temp
+    return {}
+
+
+def request_weather(latitute, long):
+    weather_url = WEATHER_API_BASE.format(lat=latitute, long=long)
+    try:
+        r = requests.get(weather_url)
+        r.raise_for_status()
+        return r.json()
     except Exception as e:
-        print(e)
-        return {}
+        logger.error("Error getting weather")
+        logger.error(e)
+        return None
