3091b07be00ab951500c637309b93648de26fb2f
dalia daud <daliadaud@gmail.com>
2021-06-06 21:47:20 +0800

    refactor, remvoed test code

--- weather/views.py
+++ weather/views.py
@@ -16,13 +16,11 @@ from .consts import CITIES_INFO, DEFAULT_CITY
 def current_weather(request):
     cities = request.GET.getlist('city', [DEFAULT_CITY])
     data = get_my_cities_weather(cities)
-    print(data)
     return render(request, 'weather/index.html', {"weather": data})
 
 
 def forecast_weather(request):
     city = request.GET.get('city', [DEFAULT_CITY])
-    print("getting forecast **********")
     data = get_forecast(city)
     return render(request, 'weather/forecast.html', {"weather": data})
 
